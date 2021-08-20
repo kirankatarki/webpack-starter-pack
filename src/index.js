8bfb9d43d6a0c70bccd5e4d33f3efe74c8b7397c
kirankatarki <kirankat07@gmail.com>
2021-08-20 13:03:12 +0530

    output-management

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import _ from 'lodash';
-import './style.scss';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js'
+// import './style.scss';
+// import Icon from './icon.png';
+// import Data from './data.xml';
+// import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
-  
+    const btn = document.createElement('button');
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
+    // element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    // const myIcon = new Image();
+    // myIcon.src = Icon;
   
-    element.appendChild(myIcon);
+    // element.appendChild(myIcon);
 
-    console.log(Data);
-    console.log(Notes);
+    // console.log(Data);
+    // console.log(Notes);
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,56 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Check Management',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
-  module: {
-    rules: [
-    //   {
-    //     test: /\.css$/i,
-    //     use: ['style-loader', 'css-loader', 'sass-loader'],
-    //   },
-    {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'css-loader'  // <-- assets are identified here
-          }, {
-            loader: 'resolve-url-loader'  // <-- receives CSS and source-map from SASS compile
-          }, {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,  // <-- IMPORTANT!
-            }
-          }
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
+  // module: {
+  //   rules: [
+  //   //   {
+  //   //     test: /\.css$/i,
+  //   //     use: ['style-loader', 'css-loader', 'sass-loader'],
+  //   //   },
+  //   {
+  //       test: /\.scss$/,
+  //       use: [
+  //         {
+  //           loader: 'css-loader'  // <-- assets are identified here
+  //         }, {
+  //           loader: 'resolve-url-loader'  // <-- receives CSS and source-map from SASS compile
+  //         }, {
+  //           loader: 'sass-loader',
+  //           options: {
+  //             sourceMap: true,  // <-- IMPORTANT!
+  //           }
+  //         }
+  //       ],
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  //       type: 'asset/resource',
+  //     },
+  //     {
+  //       test: /\.(csv|tsv)$/i,
+  //       use: ['csv-loader'],
+  //     },
+  //     {
+  //       test: /\.xml$/i,
+  //       use: ['xml-loader'],
+  //     },
         
-    ],
-  },
+  //   ],
+  // },
 };
